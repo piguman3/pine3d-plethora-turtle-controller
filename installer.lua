5b5f319f18d0c6d0532dfb244c27e8f448f8e5a3
Piguman <113848275+piguman3@users.noreply.github.com>
2023-08-09 13:50:49 -0300

    Add files via upload

---
+++ installer.lua
@@ -0,0 +1,9 @@
+--Installer
+shell.run("wget https://raw.githubusercontent.com/piguman3/pine3d-plethora-turtle-controller/main/Pine3D-minified.lua")
+shell.run("wget https://raw.githubusercontent.com/piguman3/pine3d-plethora-turtle-controller/main/pine.lua")
+shell.run("wget https://raw.githubusercontent.com/piguman3/pine3d-plethora-turtle-controller/main/betterblittle.lua")
+shell.run("wget https://raw.githubusercontent.com/piguman3/pine3d-plethora-turtle-controller/main/say.lua")
+shell.run("wget https://raw.githubusercontent.com/piguman3/pine3d-plethora-turtle-controller/main/models/turtle")
+shell.run("mv turtle models/turtle")
+
+
